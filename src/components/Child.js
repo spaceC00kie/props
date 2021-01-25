32cf0329a422983acaf2d497d1204f3f78717a10
spaceC00kie <kirstie317@gmail.com>
2021-01-24 23:06:41 -0500

    working with promises

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from "react"
 
-const Child = ({person, name, lastName}) => {
-    return (
-        <div>
-            {person}
-            {name}
-            {lastName}
-        </div>
-    )
+const Child = ({ person, name, lastName }) => {
+  //   console.log("synchronous hey")
+  //   console.log("synchronous hey")
+  //   console.log("synchronous hey")
+  //   console.log("synchronous hey")
+
+  new Promise(() => {console.log("first hey")})
+    .then(() => {
+      console.log("second hey")
+      return "third hey, but from second promise"
+    })
+    .then((hey) => console.log(hey))
+  //   console.log("synchronous hey")
+  //   console.log("synchronous hey")
+  //   console.log("synchronous hey")
+  //   console.log("synchronous hey")
+  //   console.log("synchronous hey")
+  //   console.log("synchronous hey")
+
+  return (
+    <div>
+      {person}
+      {name}
+      {lastName}
+    </div>
+  )
 }
 
 export default Child
